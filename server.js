18f2d3e0f20354514898a7ccfac622f8a3602128
Dominique d'Argent <dominique.dargent@me.com>
2013-04-28 02:13:59 +0200

    Add first version of project.
    Add .gitignore file.
    Rename index.js to server.js so we ca just run `npm start` to start the server.

---
+++ server.js
@@ -0,0 +1,51 @@
+'use strict';
+
+var http 	= require('http'),
+	fs 	 	= require('fs'),
+	io 		= require('socket.io'),
+	port 	= 8080,
+	server;
+
+server = http.createServer(function(req, res) {
+	console.log(new Date() + ": " + req.method + " " + req.url);
+
+	if (req.url === "/socket") {
+	}
+	else {
+      	var file = "public" + ((req.url === "/") ? "/index.html" : req.url);
+		fs.exists(file, function(exists) {
+			if (exists) {
+				fs.readFile(file, function(error, content) {
+					if (error) {
+						res.writeHead(500);
+						res.end();
+					}
+					else {
+						res.writeHead(200, {
+                			"Content-Type": 'text/html'
+              			});
+						res.end(content);
+					}
+				});
+			}
+			else {
+				res.writeHead(404);
+				res.end();
+			}
+		});
+	}
+
+
+}).listen(port, function() {
+    console.log((new Date()) + ": ws-paint started on port " + port);
+});
+
+io.listen(server).sockets.on('connection', function(socket) {
+	// socket.emit('data', {point: {x: 10, y: 40}});
+  	socket.on('data', function (data) {
+    	console.log(data);
+		socket.broadcast.emit('data', data);
+  	});
+});
+
+
